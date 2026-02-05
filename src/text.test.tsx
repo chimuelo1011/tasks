66e4a18ea67d3d094b2a0f4e6fa309fbd8b2a4fb
chimuelo1011 <60589328+chimuelo1011@users.noreply.github.com>
2026-02-04 22:10:16 -0500

    Added "Hello World"

--- src/text.test.tsx
+++ src/text.test.tsx
@@ -6,4 +6,6 @@ test("renders the text 'Hello World' somewhere", () => {
     render(<App />);
     const texts = screen.getAllByText(/Hello World/);
     expect(texts.length).toBeGreaterThanOrEqual(1);
+
+    ("Hello Wordl");
 });
