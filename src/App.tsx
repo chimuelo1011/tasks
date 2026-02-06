9abf2c141a8c02d23ebd74c4334499e80600dc53
JeremiasBranaTonelli <jerebt@udel.edu>
2026-02-06 02:11:10 -0500

    Fixed errors shown in website

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import wisin from "./wisin.jpg";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header" style={{ backgroundColor: "red" }}>
                 <h1>
-                    <span style={{ color: "red" }}>
+                    <span style={{ color: "blue" }}>
                         {" "}
                         UD CISC275 with React Hooks and TypeScript
                     </span>
                 </h1>
+
+                <ul>
+                    <li>Ri</li>
+                    <li>cardo</li>
+                    <li>Fort</li>
+                </ul>
             </header>
-            <img
-                src="C:\Users\JBT10\tasks\src\wisin.jpg"
-                alt="wisin y yandel"
-            />
+            <img src={wisin} alt="wisin y yandel" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jeremias Brana Tonelli. Hello World
             </p>
-            <ul>
-                <li>Ri</li>
-                <li>cardo</li>
-                <li>Fort</li>
-            </ul>
+
             <Button
                 onClick={() => {
                     console.log("Hello World!");
@@ -33,6 +33,29 @@ function App(): React.JSX.Element {
             >
                 Log Hello World
             </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                height: "120px",
+                                width: "180px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                height: "120px",
+                                width: "180px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
