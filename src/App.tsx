a8fb7ced79df7f4d92a8bca370c5c555a3a11c26
JeremiasBranaTonelli <jerebt@udel.edu>
2026-02-06 01:33:12 -0500

    Finished CSS and HTML Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+            <header className="App-header" style={{ backgroundColor: "red" }}>
+                <h1>
+                    <span style={{ color: "red" }}>
+                        {" "}
+                        UD CISC275 with React Hooks and TypeScript
+                    </span>
+                </h1>
             </header>
+            <img
+                src="C:\Users\JBT10\tasks\src\wisin.jpg"
+                alt="wisin y yandel"
+            />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Jeremias Brana Tonelli. Hello World
             </p>
+            <ul>
+                <li>Ri</li>
+                <li>cardo</li>
+                <li>Fort</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
         </div>
     );
 }
